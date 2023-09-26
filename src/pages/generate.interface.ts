4df8348fbf4d6f92fe467c09419b7d98b10ed7e9
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-09-26 19:03:25 +0900

    feat:API呼び出しを行いその内容を表示、簡単なCSSの適用

---
+++ src/pages/generate.interface.ts
@@ -0,0 +1,4 @@
+export interface gptResType {
+    id: number;
+    flow: string[];
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,58 @@
 import { Inter } from 'next/font/google'
+import { Main } from 'next/document';
+import { useEffect, useState } from 'react'
+import { gptResType } from './generate.interface';
+import styles from '../styles/flow.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [data, setdata] = useState<gptResType | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/generate")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => setdata(data))
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error.message);
+      });
+  }, []);
+  
+
   return (
     <div>
-      <h1>Hello TypeScript!!</h1>
+      <header>
+        <h1>RoadMapper</h1>
+      </header>
+      <div>
+        <div>
+          <i className='gptIcon'><img src="" alt="" /></i>
+          <input type="text" />
+          <button><img src="" alt="" /></button>
+        </div>
+        <div className={styles.flow_design01}>
+          <ul className={styles.flow01}>
+            {data?.flow.map((content, index) => {
+              return (
+                <li key={index}>
+                  <dl>
+                    <dt><span className={styles.icon01}>STEP&nbsp;1</span>お申し込み</dt>
+                    <dd>{content}</dd>
+                  </dl>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+      <footer>
+
+      </footer>
     </div>
   )
 }
