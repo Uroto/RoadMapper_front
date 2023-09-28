b23f2f42dc4e3e9fe1a3f7da5903a7b96f015432
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-09-29 02:13:57 +0900

    feat:Sassでデザイン追加,UIkitを使おうとしたがNext.jsと相性が悪かったみたいで断念

---
+++ src/pages/Header.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/header.module.scss'
+
+export default function Header() {
+    return (
+        <div id={styles.header}>
+            <h2>RoadMapper</h2>
+        </div>
+    )
+}
